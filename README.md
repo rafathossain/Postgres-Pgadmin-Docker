6d792b8967c8207378abd2d06d7c397fed4249f9
rafathossain <rafathossain96@gmail.com>
2022-10-02 17:29:30 +0600

    Release 1.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# Postgres and Pgadmin Docker
+
+Steps to install:
+1. Copy the env.example to .env
+2. Fill up the following fields
+```
+POSTGRES_PASSWORD=
+
+PGADMIN_DEFAULT_EMAIL=
+PGADMIN_DEFAULT_PASSWORD=
+```
+3. Now run `sh deploy.sh`
+4. After the docker container is up, you can access the pgadmin via: {SERVER_IP}:5480
+5. Thank you.
---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Deploying Postgres Database and Pgadmin"
+echo "========================================="
+echo "Prepared by Md. Rafat Hossain"
+echo "Date: 02 October 2022"
+echo "========================================="
+
+echo -n "Cleaning up existing postgres docker containers..."
+
+docker stop $(docker ps -a -q --filter "name=postgres_db") >/dev/null 2>&1
+docker kill $(docker ps -q --filter "name=postgres_db") >/dev/null 2>&1
+docker rm $(docker ps -a -q --filter "name=postgres_db") >/dev/null 2>&1
+docker rmi $(docker images -q --filter "name=postgres_db") >/dev/null 2>&1
+
+docker stop $(docker ps -a -q --filter "name=postgres_admin") >/dev/null 2>&1
+docker kill $(docker ps -q --filter "name=postgres_admin") >/dev/null 2>&1
+docker rm $(docker ps -a -q --filter "name=postgres_admin") >/dev/null 2>&1
+docker rmi $(docker images -q --filter "name=postgres_admin") >/dev/null 2>&1
+
+docker system prune -a -f >/dev/null 2>&1
+docker volume prune -f >/dev/null 2>&1
+docker builder prune -a -f >/dev/null 2>&1
+
+shopt -s extglob
+
+rm -rf /var/lib/docker
+
+systemctl restart docker
+
+echo "COMPLETE"
+
+echo -n "Creating docker data directory..."
+
+mkdir -p /home/docker-storage/postgresql >/dev/null 2>&1
+mkdir -p /home/docker-storage/pgadmin >/dev/null 2>&1
+chmod 777 /home/docker-storage/pgadmin
+
+echo "COMPLETE"
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+echo -n "Bringing up docker container..."
+
+docker-compose -f "$SCRIPT_DIR/docker-compose.yml" up -d
+
+docker system prune -a -f >/dev/null 2>&1
+
+echo "COMPLETE"
+
+cp -n "$SCRIPT_DIR/env.example" "$SCRIPT_DIR/.env"
+
+echo "Deployment completed successfully."
+echo "========================================="
